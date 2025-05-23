48ee4d729ec3038eec233a53fac4ae1f67293f79
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-23 14:53:53 -0300

    teste conflito com o CORS

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -9,7 +9,7 @@ export default function useFetch() {
     useEffect(() => {
   async function FetchData() {
     try {
-      const response = await fetch("/api/v2/list");
+      const response = await fetch("https://desafio-galeria-de-imagem-02.vercel.app/api/v2/list");
 
       if (!response.ok) {
         throw new Error(`Erro na resposta: ${response.status}`);
