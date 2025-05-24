2b3579fc6f2b50ce757f6a2dd749cf10417cf6a5
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-24 16:07:29 -0300

    aplicando estilo e personalizando

--- src/components/photosDisplay.jsx
+++ src/components/photosDisplay.jsx
@@ -1,13 +1,14 @@
 
 
-export default function photoDisplay({key,author,url, width}) {
+export default function PhotoDisplay({id,author,url, width}) {
     return (
-        <div key={key}>
-             <img src={url} alt={author} />
-             <article>
-                    <h2> Autor: {author} </h2>
-                    <p> Dimensions: {width} </p>
-                    <p>ID: {key} </p>
+        <div key={id} className="bg-gray-200 display-card rounded-md">
+             <img src={url} alt={author} className="rounded-t-md" />
+             <article className="p-5">
+                    <h2> <span className="font-medium">Autor:</span>   {author} </h2>
+                    <p><span className="font-medium">Dimensions: </span>   {width} </p>
+                    <p> <span className="font-medium">ID:</span>   {id} </p>
+                    <button>Favorito</button>
              </article>
         </div>
     )
